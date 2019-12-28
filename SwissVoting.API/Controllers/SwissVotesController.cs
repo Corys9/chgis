d2ed46a5bd3a3cf2aca61311daf3d99ec85d8f04
Teodor Trifunov <teodor.trifunov@nuvoxx.com>
2019-12-28 20:39:33 +0100

    Add project files.

---
+++ SwissVoting.API/Controllers/SwissVotesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SwissVoting.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SwissVotesController : ControllerBase
+    {
+        private readonly ILogger<SwissVotesController> _logger;
+
+        public SwissVotesController(ILogger<SwissVotesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Votes> Get()
+        {
+            
+        }
+    }
+}
---
+++ SwissVoting/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿var CH = (function (o) {
+    o.BaseURL = "http://localhost:20001/geoserver/ows";
+    o.SharedFragment = "?service=WFS&version=1.1.0&request=GetFeature&outputFormat=json&srsName=EPSG:4326";
+    o.CantonsURL = o.BaseURL + o.SharedFragment + "&typeName=ch-cantons";
+    o.PlacesURL = o.BaseURL + o.SharedFragment + "&typeName=ch-places";
+    o.Map = null;
+
+    o.Init = function () {
+        o.Map = L.map("map")
+            .setView([46.6, 8.2], 8);
+
+        $.get(o.CantonsURL, function (data) {
+            var geoJsonLayer = new L.GeoJSON(data.features, {
+                onEachFeature: function (feature, layer) {
+                    if (feature.properties && feature.properties.name_1)
+                        layer.bindTooltip("<div class=\"canton-name\">" + feature.properties.name_1 + "</div>", { closeButton: false });
+                }
+            })
+                .addTo(o.Map);
+        });
+
+        /*$.get(o.PlacesURL, function (data) {
+            var geoJsonLayer = new L.GeoJSON(data.features)
+                .addTo(o.Map);
+        });*/
+    };
+
+    return o;
+})(CH || {});
